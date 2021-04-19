edf3879758c07a3d8e22fc8888b6d0726ca0d781
huutoan <huutoan2603@gmail.com>
2021-04-19 17:13:35 +0700

    Update to file

--- Attendance_App/Attendance_App/ViewControllers/LoginViewController.swift
+++ Attendance_App/Attendance_App/ViewControllers/LoginViewController.swift
@@ -10,25 +10,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var usernameTextField: UITextField!
-    
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    @IBOutlet weak var LoginButton: UIButton!
-    
-    @IBOutlet weak var ImageLogin: UIImageView!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        ImageLogin.image = UIImage(named: "book")
-        setUpLogin()
-    }
-    
-    func setUpLogin() {
-        styleLogin.styleTextField(usernameTextField)
-        styleLogin.styleTextField(passwordTextField)
-        styleLogin.styleFilledButton(LoginButton)
         
     }
-
 }
--- Attendance_App/Attendance_App/ViewControllers/ViewController.swift
+++ Attendance_App/Attendance_App/ViewControllers/ViewController.swift
@@ -12,33 +12,68 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageHome: UIImageView!
     
-    @IBOutlet weak var loginHome: UIButton!
+    @IBOutlet weak var UsernameTextField: UITextField!
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var StackLogin: UIStackView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         imageHome.image = UIImage(named: "book")
         setUpComponets()
+        Curved()
         // Chạy đồng bộ làm cho ứng dụng mượt hơn. Thực hiện delay trước khi chạy một khoảng thời gian truyền vào
         super.viewDidLayoutSubviews()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5 , execute: {
-            self.animation()
-        })
-    }
-    
-    // Hàm animation
-    func animation() {
-        UIView.animate(withDuration: 1.0, delay: 0, options: .curveLinear, animations: {
-            //self.loginHome.center.y = self.view.bounds.height - 100
-            self.loginHome.layoutIfNeeded()
-
+        
+        // Hàm animation
+        UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut , animations: {
+        self.StackLogin.center.y -= self.view.bounds.height - 100
+        self.StackLogin.layoutIfNeeded()
         }, completion: nil)
     }
+    
+
     func setUpComponets() {
-        styleLogin.styleFilledButton(loginHome)
-        styleLogin.shadowView(loginHome)
+        styleLogin.styleTextField(UsernameTextField)
+        styleLogin.styleTextField(PasswordTextField)
+        
+        // Login
+        styleLogin.styleFilledButton(LoginButton)
+        styleLogin.shadowView(LoginButton)
+        
+        //Stack
+        styleLogin.shadowView(StackLogin)
     }
 
+    
+    
+    func Curved (amplitude:CGFloat = 100)
+    {
+        // circlecurve
+        let universalSize = self.view.bounds
+        let baseLine = self.view.bounds.height/1.2
+        let path = UIBezierPath();
+        path.move(to: CGPoint(x: 0, y: baseLine))
+        path.addCurve(to: CGPoint(x: 1 * universalSize.width, y: baseLine ), controlPoint1: CGPoint(x: universalSize.width * (0.3) , y: amplitude + baseLine), controlPoint2: CGPoint(x: universalSize.width * (0.7), y:-amplitude + baseLine))
+        
+        
+        path.addLine(to: CGPoint(x: universalSize.width, y: universalSize.height))
+        path.addLine(to: CGPoint(x: 0, y: universalSize.height))
+        
+        
+        let layer = CAShapeLayer()
+        layer.fillColor = UIColor.init(red: 0.07, green: 0.93, blue: 0.75, alpha: 1.00).cgColor
+        layer.opacity = 0.4
+        layer.path = path.cgPath
+        self.view.layer.addSublayer(layer)
+        
+        // animation
+    }
 
 }
 
