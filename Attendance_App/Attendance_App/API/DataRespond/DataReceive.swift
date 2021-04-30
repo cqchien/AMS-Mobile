29e63b5c024d72976d8295bfd551fa9a13db749c
HungDangQuang <hungdangkg@gmail.com>
2021-04-30 23:27:28 +0700

    Commit Data receive again

---
+++ Attendance_App/Attendance_App/API/DataRespond/DataReceive.swift
@@ -0,0 +1,12 @@
+//
+//  DataReceive.swift
+//  Attendance_App
+//
+//  Created by QUANG HUNG on 30/Apr/2021.
+//  Copyright © 2021 J.K. All rights reserved.
+//
+
+import Foundation
+struct DataReceive {
+    let token: Token?
+}
