ab23af35732d521d11e6c8fe8b88be459ed00ecc
HungDangQuang <hungdangkg@gmail.com>
2021-05-07 17:29:10 +0700

    adđ table view and table view cell to display user information

---
+++ Attendance_App/Attendance_App/User Class Info/INFO_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  INFO_TableViewCell.swift
+//  Attendance_App
+//
+//  Created by QUANG HUNG on 07/May/2021.
+//  Copyright © 2021 J.K. All rights reserved.
+//
+
+import UIKit
+
+class INFO_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Attendance_App/Attendance_App/User Class Info/INFO_ViewController.swift
+++ Attendance_App/Attendance_App/User Class Info/INFO_ViewController.swift
@@ -17,34 +17,37 @@ class INFO_ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        view.backgroundColor = hexStringToUIColor(hex: "#0CC1A2")
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [hexStringToUIColor(hex: "#0CC1A2").cgColor]
-        view.layer.addSublayer(gradientLayer)
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = view.bounds
+//        gradientLayer.colors = [hexStringToUIColor(hex: "#0CC1A2").cgColor]
+//        view.layer.addSublayer(gradientLayer)
+        
+        GetUserInfo()
     }
     
 
     
 
 }
-func hexStringToUIColor (hex:String) -> UIColor {
-    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
 
-    if (cString.hasPrefix("#")) {
-        cString.remove(at: cString.startIndex)
-    }
-
-    if ((cString.count) != 6) {
-        return UIColor.gray
-    }
-
-    var rgbValue:UInt64 = 0
-    Scanner(string: cString).scanHexInt64(&rgbValue)
-
-    return UIColor(
-        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-        alpha: CGFloat(1.0)
-    )
-}
+//func hexStringToUIColor (hex:String) -> UIColor {
+//    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+//
+//    if (cString.hasPrefix("#")) {
+//        cString.remove(at: cString.startIndex)
+//    }
+//
+//    if ((cString.count) != 6) {
+//        return UIColor.gray
+//    }
+//
+//    var rgbValue:UInt64 = 0
+//    Scanner(string: cString).scanHexInt64(&rgbValue)
+//
+//    return UIColor(
+//        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+//        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+//        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+//        alpha: CGFloat(1.0)
+//    )
+//}
