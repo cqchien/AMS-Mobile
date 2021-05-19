392a8a1c1ed7c9a0577c4e0aca7f36c54841c886
HungDangQuang <hungdangkg@gmail.com>
2021-05-19 17:24:23 +0700

    Add explanation to view controller (function login)

--- Attendance_App/Attendance_App/VIEW/LoginView/ViewController.swift
+++ Attendance_App/Attendance_App/VIEW/LoginView/ViewController.swift
@@ -18,24 +18,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // Spinner loading 
     @IBOutlet weak var spinner: UIActivityIndicatorView!
-    // Action login
     
+    // Action login
     @IBAction func LoginUser(_ sender: Any) {
         
         // Check user name and password is nil or not
         if validation.validate(userName: UsernameTextField, password: PasswordTextField) == 1 {
+            
+            // Start spinning
             spinner.startAnimating()
             
+            // Call api
             post.APIRequest(email: UsernameTextField.text!, password: PasswordTextField.text!)
             
+            // Set timer for wating result of calling api
             _ = Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { (timer) in
                 
+                // Stop spinning
                 self.spinner.stopAnimating()
                 
+                // Nofitication
                 let alert = UIAlertController(title: self.post.title, message: self.post.message, preferredStyle: .alert)
-
+                
+                // Add button for this notification
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: self.handleHttpStatus))
                 
+                // Display nofitication
                 self.present(alert, animated: true, completion: nil)
             }
         }
@@ -44,6 +52,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    
+    // Get http status
     func handleHttpStatus(action: UIAlertAction) {
         if post.status <= 300 {
             validation.TransitionHome(storyboard: self.storyboard!, view: self.view)
@@ -108,13 +118,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-//    func ErrorValid()
-//    {
-//        let Myalert = UIAlertController(title: "Error", message: "Email or password incorrect", preferredStyle: .alert)
-//        Myalert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in}))
-//        present(Myalert, animated: true, completion: nil)
-//    }
-
 }
 
 
